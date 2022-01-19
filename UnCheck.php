e2ff6595a099fae69dc5c345677265edfee36a5f
Andy <Andy790620231@gmail.com>
2022-01-19 23:59:46 +0800

    部分確認

--- UnCheck.php
+++ UnCheck.php
@@ -779,9 +779,6 @@ ability_card_wretchedswarm,驚駭蟲群
 ================================================================================================================================================
 adventure_achievement_1_1_controlallies_desc,對盟友使用30次操控技能
 
-
-adventure_achievement_1_1_infuselight,光輝之神注視著我們！
-
 adventure_achievement_1_1_killbanditarchers_1,觸發陷阱
 
 adventure_achievement_1_1_killbanditarchers_2,桃心殺手
